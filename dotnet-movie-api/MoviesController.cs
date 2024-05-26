5ab0697a316a267969d1e72bce958adbba2d3ad4
Mike <allegrotech89@gmail.com>
2024-05-26 15:00:31 +0100

    Add project files.

---
+++ dotnet-movie-api/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_movie_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly OmdbApiClient _omdbApiClient;
+
+        public MoviesController(OmdbApiClient omdbApiClient)
+        {
+            _omdbApiClient = omdbApiClient;
+        }
+
+        // GET api/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var movie = await _omdbApiClient.GetMovieByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        // GET api/search/{title}
+        [HttpGet("search/{title}")]
+        public async Task<IActionResult> Search(string title)
+        {
+            var searchResult = await _omdbApiClient.GetMovieByTitle(title);
+            Console.WriteLine(searchResult);
+            Console.WriteLine("Hellow world");
+
+            if (searchResult == null || searchResult == null )
+            {
+                return NotFound();
+            }
+            return Ok(searchResult);
+        }
+
+       
+
+    }
+}
---
+++ dotnet-movie-api/OmdbHttpClient.cs
@@ -0,0 +1,40 @@
+﻿
+using Newtonsoft.Json;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+
+public class OmdbApiClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey = "dc4771ca"; // Replace with your IMDb API key
+
+    public OmdbApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Movie> GetMovieByIdAsync(string id)
+    {
+        var response = await _httpClient.GetStringAsync($"http://www.omdbapi.com/?apikey={_apiKey}&i={id}");
+        return JsonConvert.DeserializeObject<Movie>(response);
+    }
+
+    public async Task<MovieSearchResult> SearchMoviesAsync(string keyword)
+    {
+        Console.WriteLine(keyword);
+        var response = await _httpClient.GetStringAsync($"http://www.omdbapi.com/?apikey={_apiKey}&t={keyword}");
+        return JsonConvert.DeserializeObject<MovieSearchResult>(response);
+    }
+
+    public async Task<Movie> GetMovieByTitle(string title)
+    {
+        var response = await _httpClient.GetStringAsync($"http://www.omdbapi.com/?apikey={_apiKey}&t={title}");
+        return JsonConvert.DeserializeObject<Movie>(response);
+    }
+}
+
+public class MovieSearchResult
+{
+    public List<Movie> Results { get; set; }
+}
+
+
